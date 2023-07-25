57ceb80eef76082362c06017290365e9e1cd3aab
Tuan1409 <tuannvse160549@fpt.edu.vn>
2023-07-25 23:17:58 +0700

    ministore

---
+++ Desktop/New folder (4)/SWPdemo40/src/java/DTO/Product.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author kienb
+ */
+public class Product {
+    private int productID;
+    private String productLink;
+    private String productName;
+    private double productPrice;
+    private double discount;
+    private Category category;
+    private int employeeID;
+    private int quantity;
+    private boolean status;
+
+    public Product() {
+    }
+
+    public Product(int productID, String productLink, String productName, double productPrice, double discount, Category category, int employeeID, int quantity, boolean status) {
+        this.productID = productID;
+        this.productLink = productLink;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.discount = discount;
+        this.category = category;
+        this.employeeID = employeeID;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductLink() {
+        return productLink;
+    }
+
+    public void setProductLink(String productLink) {
+        this.productLink = productLink;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + this.productID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        return this.productID == other.productID;
+    }
+
+    
+    
+}
